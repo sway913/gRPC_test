f12eda8efee944e097ae9ef4473f07a1a4b72171
zhongtianzhou <zhongtianzhou@us.com>
2022-06-29 17:03:58 +0800

    add test

--- cpp/build_test.sh
+++ cpp/build_test.sh
@@ -6,8 +6,8 @@ export MY_INSTALL_DIR=${WORK_DIR}/out_grpc
 
 export PKG_CONFIG_PATH=${MY_INSTALL_DIR}/lib/pkgconfig/:$PKG_CONFIG_PATH
 
-cd grpc/examples/cpp/helloworld
-
+# cd grpc/examples/cpp/helloworld
+cd ${WORK_DIR}/helloworld
 
 mkdir -p cmake/build
 pushd cmake/build
