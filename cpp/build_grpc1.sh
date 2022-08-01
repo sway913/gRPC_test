ffad5e73bef6e7aa5e8fef1a4c60fdf8c622a036
zhongtianzhou <zhongtianzhou@360.cn>
2022-08-01 21:19:01 +0800

    update ssl

--- cpp/build_grpc1.sh
+++ cpp/build_grpc1.sh
@@ -10,12 +10,11 @@ export PATH="$MY_INSTALL_DIR/bin:$PATH"
 
 #sudo apt-get install -y build-essential autoconf libtool pkg-config
 
-git clone --recurse-submodules -b v1.46.4 https://github.com/grpc/grpc
+#git clone --recurse-submodules -b v1.46.4 https://github.com/grpc/grpc
 
 export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
 
 cd ${WORK_DIR}/grpc
-export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig/:${PKG_CONFIG_PATH}
 
 mkdir -p cmake/build
 pushd cmake/build
@@ -26,8 +25,6 @@ cmake -DgRPC_INSTALL=ON \
       -DCMAKE_BUILD_TYPE=Release \
       -DgRPC_BUILD_TESTS=OFF \
       -DBUILD_SHARED_LIBS=OFF \
-      -DgRPC_SSL_PROVIDER=package \
-      -DgRPC_ZLIB_PROVIDER=package \
       -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
       ../..
 make -j6
